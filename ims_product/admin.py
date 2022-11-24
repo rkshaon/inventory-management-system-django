f411bbcf85cbbc979ab423ea18daf9b7fac437f3
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-11-25 01:55:19 +0600

    product - model - done

--- ims_product/admin.py
+++ ims_product/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from ims_product.models import Category
+from ims_product.models import Product
 
 
 admin.site.register(Category)
+admin.site.register(Product)
--- ims_product/models.py
+++ ims_product/models.py
@@ -5,8 +5,18 @@ class Category(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False)
     image = models.ImageField(upload_to='category', null=True, blank=True)
     is_deleted = models.BooleanField(default=False)
-    added_date_time = models.DateField(auto_now_add=True)
+    added_date_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-    
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    image = models.ImageField(upload_to='product', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_deleted = models.BooleanField(default=False)
+    added_date_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
