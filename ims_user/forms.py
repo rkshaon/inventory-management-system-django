b71106f244232d00c9959e5ac16508e61c38e8d9
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-11-25 17:58:26 +0600

    user - customer - form - done

--- ims_user/forms.py
+++ ims_user/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from ims_user.models import Supplier
+from ims_user.models import Customer
 
 
 class NewSupplierForm(forms.ModelForm):
@@ -25,3 +26,28 @@ class NewSupplierForm(forms.ModelForm):
     class Meta:
         model = Supplier
         fields = {'name', 'address', 'email', 'cell'}
+
+
+class NewSupplierForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter supplier name',
+    }), required=True)
+    address = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter full address',
+        'rows': '3',
+    }), required=False)
+    email = forms.EmailField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter e-mail address',
+    }), required=False)
+    cell = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter cell number',
+    }), required=False)
+
+    class Meta:
+        model = Customer
+        fields = {'name', 'address', 'email', 'cell'}
+        
