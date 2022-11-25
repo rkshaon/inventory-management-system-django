9b0e085a1369d40a162714ddd483c107aeb2f0e5
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-11-25 18:42:06 +0600

    inventory - sale - model - done

--- ims_inventory/models.py
+++ ims_inventory/models.py
@@ -18,10 +18,9 @@ class Purchase(models.Model):
         return 'product: ' + str(self.product.name) + ' from ' + str(self.supplier.name)
 
 
-# class Sale(models.Model):
-#     quantity = models.FloatField(default=0.0, blank=False, null=False)
-#     product = models.ForeignKey('ims_product.Product', on_delete=models.CASCADE)
-#     supplier = models.ForeignKey('ims_user.Supplier', on_delete=models.CASCADE)
+class Sale(models.Model):
+    inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0.0, blank=False, null=False)
 
-#     def __str__(self):
-#         return 'product: ' + str(self.product.name) + ' from ' + str(self.supplier.name)
+    def __str__(self):
+        return 'product: ' + str(self.inventory.product.name) + ' quantity: ' + str(self.quantity)
