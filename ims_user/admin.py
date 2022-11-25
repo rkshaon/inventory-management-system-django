aa8f8d4913f81eb522020975e8ba151c5195d879
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-11-25 17:57:25 +0600

    user - customer - model - done

--- ims_user/admin.py
+++ ims_user/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 from ims_user.models import Supplier
+from ims_user.models import Customer
 
 admin.site.register(Supplier)
+admin.site.register(Customer)
--- ims_user/models.py
+++ ims_user/models.py
@@ -11,3 +11,15 @@ class Supplier(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    address = models.TextField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    cell = models.CharField(max_length=500, null=True, blank=True)
+    is_deleted = models.BooleanField(default=False)
+    added_date_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
