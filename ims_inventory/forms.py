777cdafc5c4e3d46471d3d7d9d649d5128353a56
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-11-25 18:45:40 +0600

    inventory - sale - form - done

--- ims_inventory/forms.py
+++ ims_inventory/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from ims_product.models import Product
 from ims_user.models import Supplier
 from ims_inventory.models import Purchase
+from ims_inventory.models import Inventory
+from ims_inventory.models import Sale
 
 
 class NewPurchaseForm(forms.ModelForm):
@@ -20,3 +22,17 @@ class NewPurchaseForm(forms.ModelForm):
     class Meta:
         model = Purchase
         fields = {'quantity', 'product'}
+
+
+class NewSaleForm(forms.ModelForm):
+    quantity = forms.DecimalField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter quantity of the product',
+    }), required=True)
+    inventory = forms.ModelChoiceField(queryset=Inventory.objects.all(), widget=forms.Select(attrs={
+        'class': 'form-control',
+    }), required=True)
+
+    class Meta:
+        model = Sale
+        fields = {'quantity', 'inventory'}
